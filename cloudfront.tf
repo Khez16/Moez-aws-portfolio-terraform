7765ba1efd7c305f5aecafd9e19ab14b43ef5b76
Khez16 <moezkhan786@outlook.com>
2025-10-26 21:17:10 -0400

    Fixing cloudfron key

--- cloudfront.tf
+++ cloudfront.tf
@@ -58,3 +58,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 output "cloudfront_domain" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "cloudfront_distribution_id" {
+  value       = aws_cloudfront_distribution.s3_distribution.id
+  description = "CloudFront Distribution ID for cache invalidation"
+}
